563a7dbbd7a26397fe234c9bcac722c3908ac35c
kim-se-hee <saree98@nvaer.com>
2025-04-29 02:01:11 +0900

    feat: Coupon 엔티티 생성

---
+++ src/main/java/ksh/deliveryhub/coupon/entity/Coupon.java
@@ -0,0 +1,49 @@
+package ksh.deliveryhub.coupon.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String description;
+
+    private Integer discountAmount;
+
+    private LocalDateTime expireDate;
+
+    @Enumerated(EnumType.STRING)
+    private CouponStatus couponStatus;
+
+    private Long userId;
+
+    @Builder
+    private Coupon(
+        String code,
+        String description,
+        Integer discountAmount,
+        LocalDateTime expireDate,
+        CouponStatus couponStatus,
+        Long userId
+    ) {
+        this.code = code;
+        this.description = description;
+        this.discountAmount = discountAmount;
+        this.expireDate = expireDate;
+        this.couponStatus = couponStatus;
+        this.userId = userId;
+    }
+}

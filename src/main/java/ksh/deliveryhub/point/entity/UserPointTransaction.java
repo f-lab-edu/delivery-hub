e3a64bc246197ed296fb0d1637fa5ee3a7bd10e2
kim-se-hee <saree98@nvaer.com>
2025-04-29 02:21:32 +0900

    feat: User Point Transaction 엔티티 생성

---
+++ src/main/java/ksh/deliveryhub/point/entity/UserPointTransaction.java
@@ -0,0 +1,49 @@
+package ksh.deliveryhub.point.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserPointTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PointEventType pointEventType;
+
+    private Integer initialBalance;
+
+    private Integer remainingBalance;
+
+    private LocalDateTime expireDate;
+
+    private Long orderId;
+
+    private Long userPointId;
+
+    @Builder
+    private UserPointTransaction(
+        PointEventType pointEventType,
+        Integer initialBalance,
+        Integer remainingBalance,
+        LocalDateTime expireDate,
+        Long orderId,
+        Long userPointId
+    ) {
+        this.pointEventType = pointEventType;
+        this.initialBalance = initialBalance;
+        this.remainingBalance = remainingBalance;
+        this.expireDate = expireDate;
+        this.orderId = orderId;
+        this.userPointId = userPointId;
+    }
+}

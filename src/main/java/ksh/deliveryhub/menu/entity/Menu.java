f73218a9dea96e9dd2cf868d9f9dcb1c18bf2cc0
kim-se-hee <saree98@nvaer.com>
2025-04-29 01:22:48 +0900

    feat: Menu 엔티티 생성

---
+++ src/main/java/ksh/deliveryhub/menu/entity/Menu.java
@@ -0,0 +1,45 @@
+package ksh.deliveryhub.menu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Integer price;
+
+    private String image;
+
+    private Long storeId;
+
+    @Builder
+    private Menu(
+        String name,
+        String description,
+        Integer price,
+        String image,
+        Long storeId
+    ) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.storeId = storeId;
+    }
+}

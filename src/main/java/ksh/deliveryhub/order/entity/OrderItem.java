cd2ff9b624374abdc3dcea351f94b85d8f5e1535
kim-se-hee <saree98@nvaer.com>
2025-04-29 01:55:45 +0900

    feat: Order Item 엔티티 생성

---
+++ src/main/java/ksh/deliveryhub/order/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package ksh.deliveryhub.order.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    private Long orderId;
+
+    private Long menuId;
+
+    @Builder
+    private OrderItem(
+        Integer quantity,
+        Long orderId,
+        Long menuId
+    ) {
+        this.quantity = quantity;
+        this.orderId = orderId;
+        this.menuId = menuId;
+    }
+}

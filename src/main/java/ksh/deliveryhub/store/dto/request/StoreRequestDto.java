897416cb29b020ce11ad6c637745bca14f5d40a2
kim-se-hee <saree98@nvaer.com>
2025-04-30 01:21:14 +0900

    feat: 공통 페이지 요청 스펙 정의

--- src/main/java/ksh/deliveryhub/store/dto/request/StoreRequestDto.java
+++ src/main/java/ksh/deliveryhub/store/dto/request/StoreRequestDto.java
@@ -1,5 +1,6 @@
 package ksh.deliveryhub.store.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import ksh.deliveryhub.store.entity.FoodCategory;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,6 +8,10 @@ import lombok.Getter;
 @Getter
 @Builder
 public class StoreRequestDto {
+
+    @NotNull(message = "음식 카테고리는 필수입니다.")
     private FoodCategory foodCategory;
+
+    @NotNull(message = "주소는 필수입니다.")
     private String address;
 }

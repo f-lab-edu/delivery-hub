2cc270fea4038050acd15dffb90bd44965e9c30d
kim-se-hee <saree98@nvaer.com>
2025-04-30 01:25:26 +0900

    refactor: 공통 페이지 요청 스펙 도입에 따른 리팩토링

--- src/main/java/ksh/deliveryhub/store/service/StoreService.java
+++ src/main/java/ksh/deliveryhub/store/service/StoreService.java
@@ -1,11 +1,13 @@
 package ksh.deliveryhub.store.service;
 
+import ksh.deliveryhub.common.dto.request.PageRequestDto;
 import ksh.deliveryhub.common.dto.response.PageResult;
 import ksh.deliveryhub.store.dto.request.StoreRequestDto;
 import ksh.deliveryhub.store.model.Store;
 import ksh.deliveryhub.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +17,12 @@ public class StoreService {
 
     private final StoreRepository storeRepository;
 
-    public PageResult<Store> findOpenStores(StoreRequestDto request, Pageable pageable) {
+    public PageResult<Store> findOpenStores(StoreRequestDto storeRequest, PageRequestDto pageRequest) {
+        Pageable pageable = PageRequest.of(pageRequest.getPage(), pageRequest.getSize());
+
         Page<Store> storesPage = storeRepository.findOpenStores(
-                request.getAddress(),
-                request.getFoodCategory(),
+                storeRequest.getAddress(),
+                storeRequest.getFoodCategory(),
                 pageable
             )
             .map(Store::from);

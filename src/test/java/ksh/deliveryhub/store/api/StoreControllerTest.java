e866bc99e78e9bafa94dedab0c424bfc98c10b40
kim-se-hee <saree98@nvaer.com>
2025-04-30 18:49:50 +0900

    test: 가게 등록, 업데이트 API 테스트 추가

--- src/test/java/ksh/deliveryhub/store/api/StoreControllerTest.java
+++ src/test/java/ksh/deliveryhub/store/api/StoreControllerTest.java
@@ -1,5 +1,9 @@
 package ksh.deliveryhub.store.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ksh.deliveryhub.common.exception.ErrorCode;
+import ksh.deliveryhub.store.dto.request.StoreCreateRequestDto;
+import ksh.deliveryhub.store.dto.request.StoreUpdateRequestDto;
 import ksh.deliveryhub.store.entity.FoodCategory;
 import ksh.deliveryhub.store.entity.StoreEntity;
 import ksh.deliveryhub.store.repository.StoreRepository;
@@ -8,12 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,6 +31,9 @@ class StoreControllerTest {
     MockMvc mockMvc;
 
     @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
     StoreRepository storeRepository;
 
     @AfterEach
@@ -34,7 +42,7 @@ class StoreControllerTest {
     }
 
     @Test
-    public void 주문_가능한_가게_리스트를_조회한다() throws Exception {
+    public void 주문_가능한_가게_리스트를_조회하고_성공하면_200_응답을_받는다() throws Exception {
         //given
         StoreEntity targetStore1 = createStoreEntity("가게1", "서울시 강서구", FoodCategory.PIZZA, true);
         StoreEntity targetStore2 = createStoreEntity("가게2", "서울시 강서구", FoodCategory.PIZZA, true);
@@ -62,113 +70,77 @@ class StoreControllerTest {
     }
 
     @Test
-    public void 주문_가능한_가게_리스트를_조회_시_음식_카테고리는_필수이다() throws Exception {
+    public void 새로운_가게를_등록하고_성공하면_201_응답을_받는다() throws Exception{
+        //given
+        StoreCreateRequestDto request = StoreCreateRequestDto.builder()
+            .name("음식점")
+            .description("맛있는 음식점")
+            .address("서울시")
+            .phone("010-1234-5678")
+            .foodCategory(FoodCategory.PIZZA)
+            .ownerId(1L)
+            .build();
+
         //when //then
         mockMvc.perform(
-                get("/stores")
-                    .param("address", "서울시 강서구")
-                    .param("page", "0")
-                    .param("size", "3")
-            ).andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()))
-            .andExpect(jsonPath("$.message").value("음식 카테고리는 필수입니다."));
+            post("/stores")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.data.name").value(request.getName()))
+            .andExpect(jsonPath("$.data.address").value(request.getAddress()))
+            .andExpect(jsonPath("$.data.foodCategory").value(request.getFoodCategory().name()));
     }
 
     @Test
-    public void 주문_가능한_가게_리스트를_조회_시_주소는_필수이다() throws Exception {
+    public void 가게_정보를_업데이트_하고_성공하면_200_응답을_받는다() throws Exception{
+        //given
+        StoreEntity storeEntity = createStoreEntity("변경 전 이름", "변경 전 주소", FoodCategory.PIZZA, true);
+        storeRepository.save(storeEntity);
+
+        StoreUpdateRequestDto request = StoreUpdateRequestDto.builder()
+            .id(storeEntity.getId())
+            .name("변경 후 이름")
+            .description("맛있는 음식점")
+            .address("변경 후 주소")
+            .phone("010-9876-5432")
+            .build();
+
         //when //then
         mockMvc.perform(
-                get("/stores")
-                    .param("foodCategory", "PIZZA")
-                    .param("page", "0")
-                    .param("size", "3")
+                post("/stores/" + storeEntity.getId())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
             ).andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()))
-            .andExpect(jsonPath("$.message").value("주소는 필수입니다."));
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.name").value(request.getName()))
+            .andExpect(jsonPath("$.data.description").value(request.getDescription()))
+            .andExpect(jsonPath("$.data.address").value(request.getAddress()))
+            .andExpect(jsonPath("$.data.phone").value(request.getPhone()));
     }
 
     @Test
-    public void 주문_가능한_가게_리스트를_조회_시_페이지_번호는_필수이다() throws Exception {
+    public void 존재하지_않는_가게_정보를_업데이트_하면_404_응답을_받는다() throws Exception{
+        //given
+        StoreUpdateRequestDto request = StoreUpdateRequestDto.builder()
+            .id(4685415L)
+            .name("변경 후 이름")
+            .description("맛있는 음식점")
+            .address("변경 후 주소")
+            .phone("010-9876-5432")
+            .build();
+
         //when //then
         mockMvc.perform(
-                get("/stores")
-                    .param("foodCategory", "PIZZA")
-                    .param("address", "서울시 강서구")
-                    .param("size", "3")
+                post("/stores/" + 4685415L)
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
             ).andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()))
-            .andExpect(jsonPath("$.message").value("페이지 번호는 필수입니다."));
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.status").value(404))
+            .andExpect(jsonPath("$.code").value(ErrorCode.STORE_NOT_FOUND.name()));
     }
-
-    @Test
-    public void 주문_가능한_가게_리스트를_조회_시_페이지_번호는_0이상이다() throws Exception {
-        //when //then
-        mockMvc.perform(
-                get("/stores")
-                    .param("foodCategory", "PIZZA")
-                    .param("address", "서울시 강서구")
-                    .param("page", "-1")
-                    .param("size", "3")
-            ).andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()))
-            .andExpect(jsonPath("$.message").value("페이지 번호는 0 이상입니다."));
-    }
-
-    @Test
-    public void 주문_가능한_가게_리스트를_조회_시_페이지_크기는_필수이다() throws Exception {
-        //when //then
-        mockMvc.perform(
-                get("/stores")
-                    .param("foodCategory", "PIZZA")
-                    .param("address", "서울시 강서구")
-                    .param("page", "0")
-            ).andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()))
-            .andExpect(jsonPath("$.message").value("페이지 크기는 필수입니다."));
-    }
-
-    @Test
-    public void 주문_가능한_가게_리스트를_조회_시_페이지_크기는_최대_10이다() throws Exception {
-        //when //then
-        mockMvc.perform(
-                get("/stores")
-                    .param("foodCategory", "PIZZA")
-                    .param("address", "서울시 강서구")
-                    .param("page", "0")
-                    .param("size", String.valueOf(11) )
-            ).andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()))
-            .andExpect(jsonPath("$.message").value("페이지 크기는 10이하입니다."));
-    }
-
-    @Test
-    public void 주문_가능한_가게_리스트를_조회_시_페이지_크기는_양수이다() throws Exception {
-        //when //then
-        mockMvc.perform(
-                get("/stores")
-                    .param("foodCategory", "PIZZA")
-                    .param("address", "서울시 강서구")
-                    .param("page", "0")
-                    .param("size", String.valueOf(-1) )
-            ).andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()))
-            .andExpect(jsonPath("$.message").value("페이지 크기는 양수입니다."));
-    }
-
 
     private static StoreEntity createStoreEntity(String name, String address, FoodCategory foodCategory, boolean isOpen) {
         return StoreEntity.builder()

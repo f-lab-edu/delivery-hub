ee2102f28e883a0f24fb08b1f5cd3869051a77da
kim-se-hee <saree98@nvaer.com>
2025-05-01 22:24:13 +0900

    feat: 메뉴 정보 업데이트 API 테스트

--- src/test/java/ksh/deliveryhub/menu/api/MenuControllerTest.java
+++ src/test/java/ksh/deliveryhub/menu/api/MenuControllerTest.java
@@ -2,6 +2,8 @@ package ksh.deliveryhub.menu.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ksh.deliveryhub.menu.dto.request.MenuCreateRequestDto;
+import ksh.deliveryhub.menu.dto.request.MenuUpdateRequestDto;
+import ksh.deliveryhub.menu.entity.MenuEntity;
 import ksh.deliveryhub.menu.entity.MenuStatus;
 import ksh.deliveryhub.menu.repository.MenuRepository;
 import ksh.deliveryhub.store.entity.StoreEntity;
@@ -86,5 +88,71 @@ class MenuControllerTest {
             .andExpect(status().isNotFound())
             .andExpect(jsonPath("$.status").value(404))
             .andExpect(jsonPath("$.code").value("STORE_NOT_FOUND"));
+    }
+
+    @Test
+    public void 메뉴의_정보_업데이트를_성공하면_200응답을_받는다() throws Exception {
+        //given
+        StoreEntity storeEntity = StoreEntity.builder().build();
+        storeRepository.save(storeEntity);
+
+        MenuEntity menuEntity = MenuEntity.builder()
+            .name("변경 전 이름")
+            .description("변경 전 설명")
+            .menuStatus(MenuStatus.UNAVAILABLE)
+            .price(1522)
+            .image("변경 전 이미지 url")
+            .storeId(storeEntity.getId())
+            .build();
+        menuRepository.save(menuEntity);
+
+        MenuUpdateRequestDto request = MenuUpdateRequestDto.builder()
+            .name("변경 후 이름")
+            .description("변경 후 설명")
+            .menuStatus(MenuStatus.AVAILABLE)
+            .price(1000)
+            .image("변경 후 이미지 url")
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                post("/stores/{storeId}/menus/{menuId}", storeEntity.getId(), menuEntity.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request))
+            ).andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.name").value(request.getName()))
+            .andExpect(jsonPath("$.data.description").value(request.getDescription()))
+            .andExpect(jsonPath("$.data.menuStatus").value(request.getMenuStatus().name()))
+            .andExpect(jsonPath("$.data.price").value(request.getPrice()))
+            .andExpect(jsonPath("$.data.image").value(request.getImage()));
+    }
+
+    @Test
+    public void 없는_메뉴의_정보_업데이트를_하면_404응답을_받는다() throws Exception {
+        //given
+        StoreEntity storeEntity = StoreEntity.builder().build();
+        storeRepository.save(storeEntity);
+
+        MenuEntity menuEntity = MenuEntity.builder().build();
+        menuRepository.save(menuEntity);
+
+        MenuUpdateRequestDto request = MenuUpdateRequestDto.builder()
+            .name("변경 후 이름")
+            .description("변경 후 설명")
+            .menuStatus(MenuStatus.AVAILABLE)
+            .price(1000)
+            .image("변경 후 이미지 url")
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                post("/stores/{storeId}/menus/{menuId}", storeEntity.getId(), 61651651)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request))
+            ).andDo(print())
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.status").value(404))
+            .andExpect(jsonPath("$.code").value("MENU_NOT_FOUND"));
     }
 }

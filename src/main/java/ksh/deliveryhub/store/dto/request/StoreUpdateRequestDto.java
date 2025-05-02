1173f367f23c237a4aeec534ff43eb2813939794
kim-se-hee <saree98@nvaer.com>
2025-05-02 18:25:22 +0900

    feat: 가게명, 설명 요청값에 길이 제한 추가

--- src/main/java/ksh/deliveryhub/store/dto/request/StoreUpdateRequestDto.java
+++ src/main/java/ksh/deliveryhub/store/dto/request/StoreUpdateRequestDto.java
@@ -2,6 +2,7 @@ package ksh.deliveryhub.store.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import ksh.deliveryhub.store.model.Store;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +15,11 @@ public class StoreUpdateRequestDto {
     private Long id;
 
     @NotBlank(message = "가게 이름은 필수입니다.")
+    @Size(max = 15, message = "가게 이름은 최대 15자입니다.")
     private String name;
 
     @NotBlank(message = "가게 설명은 필수입니다.")
+    @Size(max = 50, message = "가게 설명은 최대 50자입니다.")
     private String description;
 
     @NotBlank(message = "가게 주소는 필수입니다.")

d75c55ba324ba5335c29695461437e8209446b36
kim-se-hee <saree98@nvaer.com>
2025-05-04 19:12:22 +0900

    feat: facade에서 장바구니에 담긴 메뉴 수량 변경 과정 흐름 조율하도록 구현

--- src/main/java/ksh/deliveryhub/cart/facade/CartFacade.java
+++ src/main/java/ksh/deliveryhub/cart/facade/CartFacade.java
@@ -4,6 +4,8 @@ import ksh.deliveryhub.cart.model.Cart;
 import ksh.deliveryhub.cart.model.CartMenu;
 import ksh.deliveryhub.cart.service.CartMenuService;
 import ksh.deliveryhub.cart.service.CartService;
+import ksh.deliveryhub.common.exception.CustomException;
+import ksh.deliveryhub.common.exception.ErrorCode;
 import ksh.deliveryhub.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,12 @@ public class CartFacade {
     public CartMenu addMenuToCart(long userId, CartMenu cartMenu) {
         Cart cart = cartService.getUserCart(userId);
         menuService.checkAvailability(cartMenu.getMenuId());
+
         return cartMenuService.addCartMenu(cart.getId(), cartMenu);
+    }
+
+    public void changeQuantity(long userId, CartMenu cartMenu) {
+        Cart cart = cartService.getUserCart(userId);
+        cartMenuService.changeQuantity(cart.getId(), cartMenu);
     }
 }

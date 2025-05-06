06ef0cc63978ed105b0147b3fd261d7a0f171857
kim-se-hee <saree98@nvaer.com>
2025-05-06 22:03:43 +0900

    refactor: 쿠폰 로그가 쿠폰의 id가 아닌 사용자 쿠폰의 id를 갖도록 변경

---
+++ src/main/java/ksh/deliveryhub/coupon/model/CouponTransaction.java
@@ -0,0 +1,44 @@
+package ksh.deliveryhub.coupon.model;
+
+import ksh.deliveryhub.coupon.entity.CouponEventType;
+import ksh.deliveryhub.coupon.entity.CouponTransactionEntity;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class CouponTransaction {
+
+    private Long id;
+    private CouponEventType eventType;
+    private LocalDateTime eventTime;
+    private Long userCouponId;
+    private Long orderId;
+    private Long userId;
+
+    public static CouponTransaction from(
+        CouponTransactionEntity couponTransactionEntity
+    ) {
+        return CouponTransaction.builder()
+            .id(couponTransactionEntity.getId())
+            .eventType(couponTransactionEntity.getEventType())
+            .eventTime(couponTransactionEntity.getEventTime())
+            .userCouponId(couponTransactionEntity.getUserCouponId())
+            .orderId(couponTransactionEntity.getOrderId())
+            .userId(couponTransactionEntity.getUserId())
+            .build();
+    }
+
+    public CouponTransactionEntity toEntity() {
+        return CouponTransactionEntity.builder()
+            .id(getId())
+            .eventType(getEventType())
+            .eventTime(getEventTime())
+            .userCouponId(getUserCouponId())
+            .orderId(getOrderId())
+            .userId(getUserId())
+            .build();
+    }
+}

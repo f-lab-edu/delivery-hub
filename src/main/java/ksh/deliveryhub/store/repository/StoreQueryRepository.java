8996c30b4a722e1c67bb4ca72fbb2ab35b82113d
kim-se-hee <saree98@nvaer.com>
2025-05-29 17:37:25 +0900

    refactor: 가게 조회 메소드에서 주소 조석능 시, 구 일치 여부로 변경

--- src/main/java/ksh/deliveryhub/store/repository/StoreQueryRepository.java
+++ src/main/java/ksh/deliveryhub/store/repository/StoreQueryRepository.java
@@ -1,5 +1,6 @@
 package ksh.deliveryhub.store.repository;
 
+import ksh.deliveryhub.rider.entity.Location;
 import ksh.deliveryhub.store.entity.FoodCategory;
 import ksh.deliveryhub.store.entity.StoreEntity;
 import org.springframework.data.domain.Page;
@@ -7,5 +8,5 @@ import org.springframework.data.domain.Pageable;
 
 public interface StoreQueryRepository {
 
-    Page<StoreEntity> findOpenStores(String address, FoodCategory foodCategory, Pageable pageable);
+    Page<StoreEntity> findOpenStores(Location location, FoodCategory foodCategory, Pageable pageable);
 }
--- src/main/java/ksh/deliveryhub/store/service/StoreService.java
+++ src/main/java/ksh/deliveryhub/store/service/StoreService.java
@@ -2,13 +2,15 @@ package ksh.deliveryhub.store.service;
 
 import ksh.deliveryhub.common.dto.request.PageRequestDto;
 import ksh.deliveryhub.common.dto.response.PageResult;
+import ksh.deliveryhub.rider.entity.Location;
+import ksh.deliveryhub.store.entity.FoodCategory;
 import ksh.deliveryhub.store.model.Store;
 
 public interface StoreService {
 
     Store registerStore(Store store);
 
-    PageResult<Store> findOpenStores(Store store, PageRequestDto pageRequest);
+    PageResult<Store> findOpenStores(FoodCategory foodCategory, Location location, PageRequestDto pageRequest);
 
     Store updateStore(Store store);
 

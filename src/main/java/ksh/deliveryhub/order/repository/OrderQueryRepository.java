0b43efe1af32f501cee209a9ce645fb7322894f4
kim-se-hee <saree98@nvaer.com>
2025-05-30 02:31:14 +0900

    feat: 주문 상태와 주문을 받은 가게의 위치를 기준으로 주문 조회 구현

---
+++ src/main/java/ksh/deliveryhub/order/repository/OrderQueryRepository.java
@@ -0,0 +1,12 @@
+package ksh.deliveryhub.order.repository;
+
+import ksh.deliveryhub.common.dto.response.PageResult;
+import ksh.deliveryhub.order.entity.OrderStatus;
+import ksh.deliveryhub.order.repository.projection.OrderWithStoreInfoProjection;
+import ksh.deliveryhub.rider.entity.Location;
+import org.springframework.data.domain.Pageable;
+
+public interface OrderQueryRepository {
+
+    PageResult<OrderWithStoreInfoProjection> findByStatusAndCurrentLocation(OrderStatus status, Location location, Pageable pageable);
+}

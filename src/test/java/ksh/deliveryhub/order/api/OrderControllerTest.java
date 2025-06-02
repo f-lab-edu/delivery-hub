f23a2495dd7d5235ab91241df1b2b028c8b102cd
kim-se-hee <saree98@nvaer.com>
2025-06-03 00:32:08 +0900

    test: 배차 대기 주문의 상세 정보 조회 API 테스트

--- src/test/java/ksh/deliveryhub/order/api/OrderControllerTest.java
+++ src/test/java/ksh/deliveryhub/order/api/OrderControllerTest.java
@@ -3,9 +3,11 @@ package ksh.deliveryhub.order.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ksh.deliveryhub.cart.entity.CartMenuEntity;
 import ksh.deliveryhub.cart.repository.CartMenuRepository;
+import ksh.deliveryhub.common.dto.response.PageResult;
 import ksh.deliveryhub.common.dto.response.SuccessResponseDto;
 import ksh.deliveryhub.coupon.entity.CouponEntity;
 import ksh.deliveryhub.coupon.entity.UserCouponEntity;
@@ -18,13 +20,19 @@ import ksh.deliveryhub.menu.entity.MenuStatus;
 import ksh.deliveryhub.menu.repository.MenuOptionRepository;
 import ksh.deliveryhub.menu.repository.MenuRepository;
 import ksh.deliveryhub.order.dto.request.OrderCreateRequestDto;
+import ksh.deliveryhub.order.dto.response.AcceptedOrderResponseDto;
 import ksh.deliveryhub.order.dto.response.OrderCreateResponseDto;
+import ksh.deliveryhub.order.entity.OrderEntity;
+import ksh.deliveryhub.order.entity.OrderItemEntity;
+import ksh.deliveryhub.order.entity.OrderStatus;
+import ksh.deliveryhub.order.repository.OrderItemRepository;
+import ksh.deliveryhub.order.repository.OrderRepository;
 import ksh.deliveryhub.point.entity.UserPointEntity;
 import ksh.deliveryhub.point.repository.UserPointRepository;
+import ksh.deliveryhub.store.entity.Address;
 import ksh.deliveryhub.store.entity.FoodCategory;
 import ksh.deliveryhub.store.entity.StoreEntity;
 import ksh.deliveryhub.store.repository.StoreRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +43,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -62,6 +72,10 @@ class OrderControllerTest {
     MenuOptionRepository menuOptionRepository;
     @Autowired
     CouponRepository couponRepository;
+    @Autowired
+    OrderRepository orderRepository;
+    @Autowired
+    OrderItemRepository orderItemRepository;
 
     @AfterEach
     void tearDown() {
@@ -72,6 +86,8 @@ class OrderControllerTest {
         menuRepository.deleteAllInBatch();
         menuOptionRepository.deleteAllInBatch();
         couponRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        orderItemRepository.deleteAllInBatch();
     }
 
     @Test
@@ -168,9 +184,78 @@ class OrderControllerTest {
         assertThat(responseDto.getTotalPrice()).isEqualTo(36000);
     }
 
+    @Test
+    public void 배차_대기_중인_주문을_조회하고_200_응답을_받는다() throws Exception {
+        //given
+        StoreEntity storeEntity = createStore();
+        storeRepository.save(storeEntity);
+
+        MenuEntity menuEntity1 = createMenu("피자", 10000, storeEntity.getId());
+        MenuEntity menuEntity2 = createMenu("치즈 피자", 15000, storeEntity.getId());
+        menuRepository.saveAll(List.of(menuEntity1, menuEntity2));
+
+        OrderEntity orderEntity1 = createOrderEntity(OrderStatus.ACCEPTED, storeEntity.getId());
+        OrderEntity orderEntity2 = createOrderEntity(OrderStatus.ACCEPTED, storeEntity.getId());
+        orderRepository.saveAll(List.of(orderEntity1, orderEntity2));
+
+        OrderItemEntity order1ItemEntity1 = createOrderItemEntity(orderEntity1.getId(), menuEntity1.getId(), 2);
+        OrderItemEntity order1ItemEntity2 = createOrderItemEntity(orderEntity1.getId(), menuEntity2.getId(), 5);
+        OrderItemEntity order2ItemEntity1 = createOrderItemEntity(orderEntity2.getId(), menuEntity1.getId(), 42);
+        orderItemRepository.saveAll(List.of(order1ItemEntity1, order1ItemEntity2, order2ItemEntity1));
+
+        //when
+        String json = mockMvc.perform(
+                get("/orders/paid")
+                    .param("city", "서울시")
+                    .param("district", "강서구")
+                    .param("lastCreatedAt", order1ItemEntity1.getCreatedAt().minusDays(1).toString())
+                    .param("page", "0")
+                    .param("size", "3")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        //then
+        JsonNode dataNode = objectMapper.readTree(json).get("data");
+
+        PageResult<AcceptedOrderResponseDto> pageResult =
+            objectMapper.readValue(
+                dataNode.traverse(),
+                new TypeReference<>() {
+                }
+            );
+
+        List<AcceptedOrderResponseDto> orders = pageResult.getContent();
+        assertThat(orders).hasSize(2)
+            .extracting("id", "storeName", "address")
+            .containsExactlyInAnyOrder(
+                tuple(orderEntity1.getId(), storeEntity.getName(), storeEntity.getAddress().toString()),
+                tuple(orderEntity2.getId(), storeEntity.getName(), storeEntity.getAddress().toString())
+            );
+        assertThat(orders.get(0).getMenus()).hasSize(2)
+            .extracting("name", "quantity")
+            .containsExactlyInAnyOrder(
+                tuple(menuEntity1.getName(), order1ItemEntity1.getQuantity()),
+                tuple(menuEntity2.getName(), order1ItemEntity2.getQuantity())
+            );
+        assertThat(orders.get(1).getMenus()).hasSize(1)
+            .extracting("name", "quantity")
+            .containsExactlyInAnyOrder(
+                tuple(menuEntity1.getName(), order2ItemEntity1.getQuantity())
+            );
+    }
+
     private static StoreEntity createStore() {
+        Address address = Address.of("서울시", "강서구", "방화동", "1로", "1동");
+
         return StoreEntity.builder()
             .foodCategory(FoodCategory.PIZZA)
+            .address(address)
+            .phone("010-1234-5678")
+            .name("음식점")
             .build();
     }
 
@@ -221,6 +306,21 @@ class OrderControllerTest {
         return UserPointEntity.builder()
             .userId(userId)
             .balance(balance)
+            .build();
+    }
+
+    private OrderEntity createOrderEntity(OrderStatus orderStatus, Long storeId) {
+        return OrderEntity.builder()
+            .orderStatus(orderStatus)
+            .storeId(storeId)
+            .build();
+    }
+
+    private OrderItemEntity createOrderItemEntity(long orderId, long menuId, int quantity) {
+        return OrderItemEntity.builder()
+            .orderId(orderId)
+            .menuId(menuId)
+            .quantity(quantity)
             .build();
     }
 
